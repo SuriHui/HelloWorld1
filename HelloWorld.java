89a7bba0587b9a021254da1ecebd5aae63f1829f
Suri Hui <suri.hui@mrjaffesclass.com>
2023-09-06 11:38:54 -0700

    DataTypes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,10 @@
-
-/**
- * Write a description of class HelloWorld here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class HelloWorld
 {
     public static void main(String[] args)
     {
         System.out.println("Hello World") ;
+        int i;
+        double f = 2.5;
+        boolean flag = true;
     }
 }
-    
-
